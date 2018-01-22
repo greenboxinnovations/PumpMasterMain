94e9ae92785941bfb9d8f1f0c6bf9e1320fda508
Akshay Bhilare <bhilareakshay1@gmail.com>
2018-01-22 17:16:52 +0530

    menu item added

--- app/src/main/java/in/greenboxinnovations/android/pumpmaster/MainActivity.java
+++ app/src/main/java/in/greenboxinnovations/android/pumpmaster/MainActivity.java
@@ -1,11 +1,16 @@
 package in.greenboxinnovations.android.pumpmaster;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import junit.framework.Test;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +32,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Ensure the right menu is setup
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    // Start your settings activity when a menu item is selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.enterReceipt) {
+            Intent settingsIntent = new Intent(this, Test.class);
+            startActivity(settingsIntent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
 }
